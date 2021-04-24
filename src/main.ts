7ca14bfc9daa9d3884db38df4a7c8c60610c4657
Ross Brown <rbrownwsws@googlemail.com>
2021-04-24 18:36:17 +0100

    Create GitHub PoC

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,102 @@
+// SPDX-License-Identifier: Apache-2.0
+
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
+import { Octokit } from "@octokit/rest";
+
+import * as child_process from "child_process";
+
+import * as path from "path";
+import * as fs from "fs";
+
+import { createGithubResolver } from "./resolver/github.js";
+
 async function run(): Promise<void> {
   try {
-    core.debug("hello world");
+    // Get the workspace directory path
+    const workspacePath = process.env.GITHUB_WORKSPACE;
+    if (workspacePath === undefined) {
+      throw { message: "GITHUB_WORKSPACE was not provided" };
+    }
+
+    // FIXME: use an action input
+    const githubToken = process.env.GITHUB_TOKEN;
+    if (githubToken === undefined) {
+      throw { message: "GITHUB_TOKEN was not provided" };
+    }
+    const octokit = github.getOctokit(githubToken) as Octokit;
+
+    const resolvers = [createGithubResolver(octokit)];
+
+    // Get make to generate package info
+    core.info("Generating pkg-info.json files...");
+    const mkPkgInfo = child_process.spawnSync("make", [
+      "-C",
+      workspacePath,
+      "pkg-info",
+    ]);
+
+    if (mkPkgInfo.error !== undefined) {
+      throw mkPkgInfo.error;
+    }
+    core.info("Done");
+
+    const crossPkgsPath = path.join(workspacePath, "cross");
+
+    const pkgInfoFiles = fs
+      .readdirSync(crossPkgsPath)
+      .map((name) => path.join(crossPkgsPath, name))
+      .filter((pkgPath) => fs.lstatSync(pkgPath).isDirectory())
+      .map((pkgPath) => path.join(pkgPath, "pkg-info.json"));
+
+    for (const pkgInfoFile of pkgInfoFiles) {
+      if (fs.existsSync(pkgInfoFile)) {
+        // TODO: Nest another try-catch so we do not abort the entire thing
+        //       on a single file failure.
+        const pkgInfoData = JSON.parse(fs.readFileSync(pkgInfoFile, "utf-8"));
+        await getLatestVersion(pkgInfoData, resolvers);
+      } else {
+        core.warning("Missing: " + pkgInfoFile);
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+async function getLatestVersion(pkgInfo: PkgInfo, resolvers: Resolver[]) {
+  // Iterate through resolvers until we find one that works
+  let resolvedVersion = null;
+  let resolverIdx = 0;
+  while (resolvedVersion === null && resolverIdx < resolvers.length) {
+    resolvedVersion = await resolvers[resolverIdx](pkgInfo);
+
+    resolverIdx++;
+  }
+
+  if (resolvedVersion !== null) {
+    if (
+      resolvedVersion.currentVersion !== resolvedVersion.latestVersionMajor ||
+      resolvedVersion.currentVersion !== resolvedVersion.latestVersionMinor ||
+      resolvedVersion.currentVersion !== resolvedVersion.latestVersionPatch
+    ) {
+      core.info(
+        pkgInfo.PKG_NAME +
+          " is OLD: " +
+          resolvedVersion.currentVersion +
+          " > " +
+          resolvedVersion.latestVersionPatch +
+          " => " +
+          resolvedVersion.latestVersionMinor +
+          " ==> " +
+          resolvedVersion.latestVersionMajor
+      );
+    } else {
+      core.info(pkgInfo.PKG_NAME + " is OK.");
+    }
+  } else {
+    core.info(pkgInfo.PKG_NAME + " is UNKNOWN.");
   }
 }
 
---
+++ src/pkginfo.ts
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+
+interface PkgInfo {
+  PKG_NAME: string;
+  PKG_VERS: string;
+  PKG_EXT: string;
+  PKG_DIST_NAME: string;
+  PKG_DIST_SITE: string;
+  PKG_DIR: string;
+  PKG_DOWNLOAD_METHOD: string;
+  PKG_GIT_HASH: string;
+}
