d0fe2b5a230d659182ef82e017b39f8890a2a9ca
Ross Brown <rbrownwsws@googlemail.com>
2021-04-25 06:41:56 +0100

    Add tests for resolver with SemVer packages

--- src/resolver.test.ts
+++ src/resolver.test.ts
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
-import { ReleaseIndexer } from "./release-indexers/release-indexer";
+import { CursorResult } from "prettier";
+import {
+  ReleaseIndexer,
+  ReleaseIndexKind,
+} from "./release-indexers/release-indexer";
 import {
   createResolver,
   ResolvedVersions,
@@ -19,6 +23,19 @@ function createDummyPkgInfo(): PkgInfo {
   };
 }
 
+function createFakeReleaseIndexer(releaseVersions: string[]): ReleaseIndexer {
+  return async (pkgInfo: PkgInfo) => {
+    return {
+      kind: ReleaseIndexKind.SUPPORTED,
+      getReleaseIterator: async function* () {
+        for (const version of releaseVersions) {
+          yield version;
+        }
+      },
+    };
+  };
+}
+
 test("Trying to resolve with no indexer results in ERR_NO_INDEXER", async () => {
   /* Given */
   const indexers: ReleaseIndexer[] = [];
@@ -30,4 +47,68 @@ test("Trying to resolve with no indexer results in ERR_NO_INDEXER", async () => 
 
   /* Then */
   expect(result.kind).toBe(ResolvedVersionsKind.ERR_NO_INDEXER);
+});
+
+describe("Testing resolution", () => {
+  describe("of SemVer", () => {
+    /* Given */
+    const indexers: ReleaseIndexer[] = [
+      createFakeReleaseIndexer([
+        "1.0.0",
+        "2.0.0",
+        "2.3.4",
+        "2.3.5",
+        "2.4.0",
+        "3.0.0",
+        "4.0.0",
+      ]),
+    ];
+    const resolve = createResolver(indexers);
+    const pkgInfo = { ...createDummyPkgInfo(), PKG_VERS: "2.3.4" };
+
+    test("Major 2.3.4 vs [1.0.0, 2.0.0, 2.3.4, 2.3.5, 2.4.0, 3.0.0, 4.0.0] = 4.0.0", async () => {
+      /* When */
+      const result: ResolvedVersions = await resolve(pkgInfo);
+
+      /* Then */
+      expect(result.kind).toBe(ResolvedVersionsKind.SUCCESS);
+
+      // Force TS to be happy
+      if (result.kind !== ResolvedVersionsKind.SUCCESS) {
+        throw "Something has gone very wrong";
+      }
+
+      expect(result.latestVersionMajor).toBe("4.0.0");
+    });
+
+    test("Minor 2.3.4 vs [1.0.0, 2.0.0, 2.3.4, 2.3.5, 2.4.0, 3.0.0, 4.0.0] = 2.4.0", async () => {
+      /* When */
+      const result: ResolvedVersions = await resolve(pkgInfo);
+
+      /* Then */
+      expect(result.kind).toBe(ResolvedVersionsKind.SUCCESS);
+
+      // Force TS to be happy
+      if (result.kind !== ResolvedVersionsKind.SUCCESS) {
+        throw "Something has gone very wrong";
+      }
+
+      expect(result.latestVersionMinor).toBe("2.4.0");
+    });
+
+    test("Patch 2.3.4 vs [1.0.0, 2.0.0, 2.3.4, 2.3.5, 2.4.0, 3.0.0, 4.0.0] = 2.3.5", async () => {
+      /* When */
+      const result: ResolvedVersions = await resolve(pkgInfo);
+
+      /* Then */
+      expect(result.kind).toBe(ResolvedVersionsKind.SUCCESS);
+
+      // Force TS to be happy
+      if (result.kind !== ResolvedVersionsKind.SUCCESS) {
+        throw "Something has gone very wrong";
+      }
+
+      expect(result.latestVersionPatch).toBe("2.3.5");
+    });
+  });
 });
